2c0e4e06fb02a6ba48afbac942464d51f607636a
LadiFlow <adi_curt@hotmail.com>
2021-03-19 16:27:11 +0100

    range of numbers

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,12 +1,21 @@
 function rangeOfNumbers(startN, endN) {
 
-    if (startN > endN){
-        return "Jebaiga";
-    }   else {
-        var numbers = rangeOfNumbers(startN, endN - 1)
-        numbers.push(endN)
-        return numbers;
+    if (startN - endN === 0){
+        return [startN];
+    }   else if (startN > endN) {
+        return "The starting number will always be less than or equal to the ending number";
+    } else {
+        var number = rangeOfNumbers(startN, endN - 1);
+        number.push(endN);
+        return number;
     }
-}
+    }
+    
 
-console.log(rangeOfNumbers(8, 5));
+
+
+console.log(rangeOfNumbers(1, 7));
+console.log(rangeOfNumbers(3, 10));
+console.log(rangeOfNumbers(5, 5));
+console.log(rangeOfNumbers(7, 5));
+module.exports = rangeOfNumbers;
